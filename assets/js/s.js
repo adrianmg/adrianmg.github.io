af902ec2c2f7e8fb66c3cfafdae6dc299bb5832f
Adrián Mato <yensamg@gmail.com>
2018-04-16 23:23:42 -0700

    Removed jQuery dependencies, now scrolling goes through vanilla js

--- assets/js/s.js
+++ assets/js/s.js
@@ -1,17 +1,75 @@
-// @todo: do this in vanilla javascript instead Smooth scroll when clicking on Menu items with anchor points
-// Home: scroll animation after click
-$("a").click(function() {
-  $("html, body").animate(
-    {
-      scrollTop: $($.attr(this, "href")).offset().top
-    },
-    500
-  );
+/*
+  HOME
+*/
+
+// Years of experience
+const yearsExperience = document.querySelector("#years");
+if (yearsExperience !== null) {
+  yearsExperience.textContent = new Date().getFullYear() - 2007;
+}
+
+// Scroll to work/projects
+const projectsLink = document.querySelector(
+  ".bio-intro-nav [href='#projects']"
+);
+
+projectsLink.addEventListener("click", function(e) {
+  scrollToItem(document.querySelector("#projects"), 500);
+
+  e.preventDefault();
   return false;
 });
 
-// Home: years of experience
-let yearsExperience = document.querySelector("#years");
-if (yearsExperience !== null) {
-  yearsExperience.textContent = new Date().getFullYear() - 2007;
+/*
+  HELPERS
+*/
+
+// Scrolling function from A -> B (modified from: https://bit.ly/2H3JKMV)
+function scrollToItem(destination, duration = 200) {
+  const start = window.pageYOffset;
+  const startTime =
+    "now" in window.performance ? performance.now() : new Date().getTime();
+
+  const documentHeight = Math.max(
+    document.body.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.clientHeight,
+    document.documentElement.scrollHeight,
+    document.documentElement.offsetHeight
+  );
+  const windowHeight =
+    window.innerHeight ||
+    document.documentElement.clientHeight ||
+    document.getElementsByTagName("body")[0].clientHeight;
+  const destinationOffset =
+    typeof destination === "number" ? destination : destination.offsetTop;
+  const destinationOffsetToScroll = Math.round(
+    documentHeight - destinationOffset < windowHeight
+      ? documentHeight - windowHeight
+      : destinationOffset
+  );
+
+  if ("requestAnimationFrame" in window === false) {
+    window.scroll(0, destinationOffsetToScroll);
+    return;
+  }
+
+  function scroll() {
+    const now =
+      "now" in window.performance ? performance.now() : new Date().getTime();
+    const time = Math.min(1, (now - startTime) / duration);
+    const timeFunction = 0.5 * (1 - Math.cos(Math.PI * time));
+    window.scroll(
+      0,
+      Math.ceil(timeFunction * (destinationOffsetToScroll - start) + start)
+    );
+
+    if (window.pageYOffset === destinationOffsetToScroll) {
+      return;
+    }
+
+    requestAnimationFrame(scroll);
+  }
+
+  scroll();
 }
