1102401900fae3d45016f078355a03c139a7e030
Adrián Mato <yensamg@gmail.com>
2018-04-16 21:00:51 -0700

    Some follow up after 4 months…

--- assets/js/s.js
+++ assets/js/s.js
@@ -1,7 +1,17 @@
-// Smooth scroll when clicking on Menu items with anchor points
-$('a').click(function(){
-  $('html, body').animate({
-    scrollTop: $( $.attr(this, 'href') ).offset().top
-  }, 500);
+// @todo: do this in vanilla javascript instead Smooth scroll when clicking on Menu items with anchor points
+// Home: scroll animation after click
+$("a").click(function() {
+  $("html, body").animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top
+    },
+    500
+  );
   return false;
 });
+
+// Home: years of experience
+let yearsExperience = document.querySelector("#years");
+if (yearsExperience !== null) {
+  yearsExperience.textContent = new Date().getFullYear() - 2007;
+}
