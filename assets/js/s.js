b7bd8db9f5500e8f081483e814f6261f40a9cf0a
Adrian Mato <yensamg@gmail.com>
2019-05-24 19:11:15 -0700

    Rollback of JS scroll behavior

--- assets/js/s.js
+++ assets/js/s.js
@@ -17,18 +17,16 @@ HOME
 */
 let arrow = document.querySelector(".home-intro-scroll");
 const arrowTreshold = 100; // when stops being visible
-const workAnchor = ".home-work-grid__project";
+const workAnchor = "#home-work";
 const navWork = document.querySelector(
   `.home-navigation [href='${workAnchor}']`
 );
 
 // click on navigation 'work' and scroll
 navWork.addEventListener("click", function(e) {
-  // scrollToItem(document.querySelector(workAnchor), 800);
-  workAnchor.scrollIntoView({behavior: 'smooth', block: 'end' });
-
-  // history.pushState({}, "", workAnchor);
-
+  scrollToItem(document.querySelector(workAnchor), 800);
+  // workAnchor.scrollIntoView({behavior: 'smooth' });
+  history.pushState({}, "", workAnchor);
   e.preventDefault();
   return false;
 });
@@ -60,58 +58,4 @@ function scrollHandler() {
       arrow = false;
     }, 400);
   }
-}
-
-/*
-  HELPERS
-*/
-
-// Scrolling function from A -> B (modified from: https://bit.ly/2H3JKMV)
-function scrollToItem(destination, duration = 500) {
-  const start = window.pageYOffset;
-  const startTime = "now" in window.performance ? performance.now() : new Date().getTime();
-
-  const documentHeight = Math.max(
-    document.body.scrollHeight,
-    document.body.offsetHeight,
-    document.documentElement.clientHeight,
-    document.documentElement.scrollHeight,
-    document.documentElement.offsetHeight
-  );
-  const windowHeight =
-    window.innerHeight ||
-    document.documentElement.clientHeight ||
-    document.getElementsByTagName("body")[0].clientHeight;
-  const destinationOffset =
-    typeof destination === "number" ? destination : destination.offsetTop;
-  const destinationOffsetToScroll = Math.round(
-    documentHeight - destinationOffset < windowHeight
-      ? documentHeight - windowHeight
-      : destinationOffset
-  );
-
-  if ("requestAnimationFrame" in window === false) {
-    window.scroll(0, destinationOffsetToScroll);
-    return;
-  }
-
-  function scroll() {
-    const now =
-      "now" in window.performance ? performance.now() : new Date().getTime();
-
-    const time = Math.min(1, (now - startTime) / duration);
-    const timeFunction = 0.5 * (1 - Math.cos(Math.PI * time));
-    window.scroll(
-      0,
-      Math.ceil(timeFunction * (destinationOffsetToScroll - start) + start)
-    );
-
-    if (Math.round(window.pageYOffset) >= destinationOffsetToScroll) {
-      return;
-    }
-
-    requestAnimationFrame(scroll);
-  }
-
-  scroll();
 }
