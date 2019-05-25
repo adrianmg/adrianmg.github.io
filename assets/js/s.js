35ae74461781dc3e003f61e86575658f675c8441
Adrian Mato <yensamg@gmail.com>
2019-05-24 19:50:03 -0700

    Improvement: Use strict in JS and clean comments

--- assets/js/s.js
+++ assets/js/s.js
@@ -1,106 +1,106 @@
-// Lazy Load of images
-var lazyLoad = new LazyLoad({
-  elements_selector: ".lazy",
-  threshold: 800
-});
+(() => {
+  'use strict';
 
-/*
-HOME
-*/
-let arrow = document.querySelector(".home-intro-scroll");
-const arrowTreshold = 100; // when stops being visible
-const workAnchor = "#home-work";
-const navWork = document.querySelector(
-  `.home-navigation [href='${workAnchor}']`
-);
+  // Lazy Load of images
+  var lazyLoad = new LazyLoad({
+    elements_selector: ".lazy",
+    threshold: 800
+  });
 
-// click on navigation 'work' and scroll
-navWork.addEventListener("click", function(e) {
-  scrollToItem(document.querySelector(workAnchor), 800);
-  history.pushState({}, "", workAnchor);
-  e.preventDefault();
-  return false;
-});
-
-// scroll hint
-function showScrollHint(seconds) {
-  if (arrow && document.scrollingElement.scrollTop <= arrowTreshold) {
-    setTimeout(function() {
-      if (arrow) {
-        arrow.classList.add("visible");
-      }
-    }, seconds * 1000);
-  }
-}
-
-// hide scroll hint
-document.addEventListener("scroll", scrollHandler);
-
-function scrollHandler() {
-  let scroll = document.scrollingElement.scrollTop;
-
-  if (scroll >= arrowTreshold && arrow) {
-    arrow.classList.remove("visible");
-
-    document.removeEventListener("scroll", scrollHandler);
-    // remove element after transition (avoid dealing with event handling + transitionend)
-    setTimeout(function() {
-      arrow.parentNode.removeChild(arrow);
-      arrow = false;
-    }, 400);
-  }
-}
-
-/*
-  HELPERS
-*/
-
-// Scrolling function from A -> B (modified from: https://bit.ly/2H3JKMV)
-function scrollToItem(destination, duration = 500) {
-  const start = window.pageYOffset;
-  const startTime = "now" in window.performance ? performance.now() : new Date().getTime();
-
-  const documentHeight = Math.max(
-    document.body.scrollHeight,
-    document.body.offsetHeight,
-    document.documentElement.clientHeight,
-    document.documentElement.scrollHeight,
-    document.documentElement.offsetHeight
-  );
-  const windowHeight =
-    window.innerHeight ||
-    document.documentElement.clientHeight ||
-    document.getElementsByTagName("body")[0].clientHeight;
-  const destinationOffset =
-    typeof destination === "number" ? destination : destination.offsetTop;
-  const destinationOffsetToScroll = Math.round(
-    documentHeight - destinationOffset < windowHeight
-      ? documentHeight - windowHeight
-      : destinationOffset
+  // HOME
+  let arrow = document.querySelector(".home-intro-scroll");
+  const arrowTreshold = 100; // when stops being visible
+  const workAnchor = "#home-work";
+  const navWork = document.querySelector(
+    `.home-navigation [href='${workAnchor}']`
   );
 
-  if ("requestAnimationFrame" in window === false) {
-    window.scroll(0, destinationOffsetToScroll);
-    return;
+  // HOME: click on navigation 'work' and scroll
+  navWork.addEventListener("click", function(e) {
+    scrollToItem(document.querySelector(workAnchor), 800);
+    history.pushState({}, "", workAnchor);
+    e.preventDefault();
+    return false;
+  });
+
+  // HOME: scroll hint
+  function showScrollHint(seconds) {
+    if (arrow && document.scrollingElement.scrollTop <= arrowTreshold) {
+      setTimeout(function() {
+        if (arrow) {
+          arrow.classList.add("visible");
+        }
+      }, seconds * 1000);
+    }
   }
 
-  function scroll() {
-    const now =
-      "now" in window.performance ? performance.now() : new Date().getTime();
+  // HOME: hide scroll hint
+  document.addEventListener("scroll", scrollHandler);
 
-    const time = Math.min(1, (now - startTime) / duration);
-    const timeFunction = 0.5 * (1 - Math.cos(Math.PI * time));
-    window.scroll(
-      0,
-      Math.ceil(timeFunction * (destinationOffsetToScroll - start) + start)
+  function scrollHandler() {
+    let scroll = document.scrollingElement.scrollTop;
+
+    if (scroll >= arrowTreshold && arrow) {
+      arrow.classList.remove("visible");
+
+      document.removeEventListener("scroll", scrollHandler);
+      // remove element after transition (avoid dealing with event handling + transitionend)
+      setTimeout(function() {
+        arrow.parentNode.removeChild(arrow);
+        arrow = false;
+      }, 400);
+    }
+  }
+
+  // HELPERS
+
+  // HELPERS: scrolling function from A -> B (modified from: https://bit.ly/2H3JKMV)
+  function scrollToItem(destination, duration = 500) {
+    const start = window.pageYOffset;
+    const startTime = "now" in window.performance ? performance.now() : new Date().getTime();
+
+    const documentHeight = Math.max(
+      document.body.scrollHeight,
+      document.body.offsetHeight,
+      document.documentElement.clientHeight,
+      document.documentElement.scrollHeight,
+      document.documentElement.offsetHeight
+    );
+    const windowHeight =
+      window.innerHeight ||
+      document.documentElement.clientHeight ||
+      document.getElementsByTagName("body")[0].clientHeight;
+    const destinationOffset =
+      typeof destination === "number" ? destination : destination.offsetTop;
+    const destinationOffsetToScroll = Math.round(
+      documentHeight - destinationOffset < windowHeight
+        ? documentHeight - windowHeight
+        : destinationOffset
     );
 
-    if (Math.round(window.pageYOffset) >= destinationOffsetToScroll) {
+    if ("requestAnimationFrame" in window === false) {
+      window.scroll(0, destinationOffsetToScroll);
       return;
     }
 
-    requestAnimationFrame(scroll);
-  }
+    function scroll() {
+      const now =
+        "now" in window.performance ? performance.now() : new Date().getTime();
 
-  scroll();
-}
+      const time = Math.min(1, (now - startTime) / duration);
+      const timeFunction = 0.5 * (1 - Math.cos(Math.PI * time));
+      window.scroll(
+        0,
+        Math.ceil(timeFunction * (destinationOffsetToScroll - start) + start)
+      );
+
+      if (Math.round(window.pageYOffset) >= destinationOffsetToScroll) {
+        return;
+      }
+
+      requestAnimationFrame(scroll);
+    }
+
+    scroll();
+  }
+})();
